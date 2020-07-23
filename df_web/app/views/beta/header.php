3bf2b7aa2d59477fb8a217061d8cd5f1c2044c42
ETY001 <work@akawa.ink>
2020-07-23 09:09:51 +0800

    update

--- df_web/app/views/beta/header.php
+++ df_web/app/views/beta/header.php
@@ -5,13 +5,15 @@
     <title>Pix</title>
     <link rel="stylesheet" type="text/css" href="/public/css/global.css">
     <link href="/public/css/bootstrap.min.css" rel="stylesheet">
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({
-          google_ad_client: "ca-pub-7536831447654223",
-          enable_page_level_ads: true
-     });
-</script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-155419471-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-155419471-2');
+    </script>
     <?php echo $header_js;?>
     <?php echo $header_css;?>
 </head>
@@ -21,7 +23,7 @@
     <ul class="nav nav-pills pull-right">
       <li <?php echo ($page_tag=='index')?'class="active"':'';?> ><a href="<?php echo _dfUrl();?>"><?php echo $data['lang']->line('beta_share_nav');?></a></li>
       <li <?php echo ($page_tag=='createworld')?'class="active"':'';?> ><a href="<?php echo _dfUrl('pix','createWorld');?>"><?php echo $data['lang']->line('beta_create_nav');?></a></li>
-      <li <?php echo ($page_tag=='advice')?'class="active"':'';?> ><a href="<?php echo _dfUrl('pix','advice');?>"><?php echo $data['lang']->line('beta_advice_nav');?></a></li>
+      <li <?php echo ($page_tag=='advice')?'class="active"':'';?> ><a href="https://akawa.ink/about/"><?php echo $data['lang']->line('beta_advice_nav');?></a></li>
     </ul>
     <h3><a href="<?php echo site_url();?>" class="muted">Pix</a></h3>
   </div>
